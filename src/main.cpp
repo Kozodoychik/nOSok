07731a8f934812ee211080d9b2ef34472333c640
Kozodoychik <dimasikanovikov@gmail.com>
2025-10-06 14:19:20 +0300

    IO + VGA

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <stdint.h>
+#include <drivers/video.h>
 
 extern "C" void kmain() {
-    *(uint8_t*)0xC03FF000 = 'A';
-    *(uint8_t*)0xC03FF001 = 0x06;
+    nosok::video::putc({'n', 0x07});
+    nosok::video::putc({'O', 0x07});
+    nosok::video::putc({'S', 0x07});
+    nosok::video::putc({'o', 0x07});
+    nosok::video::putc({'k', 0x07});
+
+    nosok::video::putc({'\n', 0x07});
+
+    nosok::video::putc({'T', 0x07});
+    nosok::video::putc({'e', 0x07});
+    nosok::video::putc({'s', 0x07});
+    nosok::video::putc({'t', 0x07});
     return;
 }
