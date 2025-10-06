eaf2d9bbdd44d5c6fe3b1fdfe1eefcfa53133f94
Kozodoychik <dimasikanovikov@gmail.com>
2025-10-06 14:16:04 +0300

    Revert "IO + VGA"
    This reverts commit 5e08dea2df76c7af899af22cff1746e09b2ee947.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include <stdint.h>
-#include <drivers/video.h>
-
 
 extern "C" void kmain() {
-    nosok::video::putc({'n', 0x07});
-    nosok::video::putc({'O', 0x07});
-    nosok::video::putc({'S', 0x07});
-    nosok::video::putc({'o', 0x07});
-    nosok::video::putc({'k', 0x07});
-
-    nosok::video::putc({'\n', 0x07});
-
-    nosok::video::putc({'T', 0x07});
-    nosok::video::putc({'e', 0x07});
-    nosok::video::putc({'s', 0x07});
-    nosok::video::putc({'t', 0x07});
-
+    *(uint8_t*)0xC03FF000 = 'A';
+    *(uint8_t*)0xC03FF001 = 0x06;
     return;
 }
